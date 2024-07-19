2a38bef2ec90aac9c1b14ad4043e5d455abd8df1
Arjun Singh <57623600+Arjdroid@users.noreply.github.com>
2024-07-19 17:05:59 +0530

    Update init.sh

--- userdata/init.sh
+++ userdata/init.sh
@@ -3,5 +3,5 @@ sudo apt update
 sudo apt install -y firewalld
 sudo systemctl start firewalld
 sudo systemctl enable firewalld
-sudo firewall-cmd --zone=public --add-port=1024-65535/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=443-65535/tcp --permanent
 sudo firewall-cmd --reload
